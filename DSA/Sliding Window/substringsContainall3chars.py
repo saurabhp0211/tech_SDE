840974cb1ff7dd6ae00e918e55e7c779a471adf3
Saurabh Pandey <saurabhkumarpandey604@gmail.com>
2026-05-26 23:34:44 +0530

    Substrings contain all three characters

---
+++ DSA/Sliding Window/substringsContainall3chars.py
@@ -0,0 +1,27 @@
+s = "abcabc"
+
+def substrContAll3(s):
+    count=0
+    l=0
+    dict1={'a':0,'b':0,'c':0}
+    k=len(s)
+    for r in range(k):
+        char=s[r]
+        dict1[char]=dict1.get(char,0)+1
+
+        while dict1['a']>0 and dict1['b']>0 and dict1['c']>0:
+            count+=(k-r)
+
+            char_l=s[l]
+            dict1[char_l]-=1
+            l+=1
+
+    return count
+
+
+
+print(substrContAll3(s))
+        
+
+
+
