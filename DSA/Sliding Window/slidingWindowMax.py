98839c612da9487c7c207088ce340c7ddf6047a1
Saurabh Pandey <saurabhkumarpandey604@gmail.com>
2026-05-29 00:50:33 +0530

    sliding window max (opmtimised)

--- DSA/Sliding Window/slidingWindowMax.py
+++ DSA/Sliding Window/slidingWindowMax.py
@@ -1,3 +1,5 @@
+import collections
+
 nums = [1,3,-1,-3,5,3,6,7]
 k=3
 
@@ -20,3 +22,20 @@ print(slidingWin(nums,k))
 
 
 # O(n)
+
+def sliding_win_Max(nums,k):
+    res=[]
+    q=collections.deque()
+
+    for r in range(len(nums)):
+        while q and nums[q[-1]]<nums[r]:
+            q.pop()
+        q.append(r)
+
+        if q[0]<(r-k+1):
+            q.popleft()
+        if r>=k-1:
+            res.append(nums[q[0]])
+    return res
+
+print(sliding_win_Max(nums,k))
