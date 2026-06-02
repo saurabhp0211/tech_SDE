f0561f8953be686c9bb708f509c68a039b253e7c
Saurabh Pandey <saurabhkumarpandey604@gmail.com>
2026-06-03 00:31:30 +0530

    final code for subarrays with k diff integers

--- DSA/Sliding Window/subarrKdiffInt.py
+++ DSA/Sliding Window/subarrKdiffInt.py
@@ -1,28 +1,30 @@
 nums = [1,2,1,2,3]
 k = 2
 
-def atmostK(nums,k):
-    l=0
-    container={}
+def subarrKdiffInts(nums,k):
+    return atmost(nums,k)-atmost(nums,k-1)
 
-    total_subarr=0
+
+def atmost(nums,k):
+    dict1={}
+    l=0
+    subarrays=0
     for r in range(len(nums)):
         char=nums[r]
-        container[char]=container.get(char,0)+1
+        dict1[char]=dict1.get(char,0)+1
 
-        while len(container)>k:
+        while len(dict1)>k:
             char_l=nums[l]
-            container[char_l]-=1
+            dict1[char_l]-=1
 
-            if container[char_l]==0:
-                del container[char_l]
+            if dict1[char_l]==0:
+                del dict1[char_l]
             l+=1
-        total_subarr+=r-l+1
-    return total_subarr
+        subarrays+=(r-l+1)
+        
+    return subarrays
 
-print(atmostK(nums,k))
-
-
+print(subarrKdiffInts(nums,k))
         
     
     
